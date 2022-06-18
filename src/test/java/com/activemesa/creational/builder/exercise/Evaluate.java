c5e072790ff4685a18a3227e8a7f6f74c806e4c8
Ethan Miller <ethan.miller@aa.com>
2022-06-18 12:48:48 -0500

    initial commit with builder pattern

---
+++ src/test/java/com/activemesa/creational/builder/exercise/Evaluate.java
@@ -0,0 +1,33 @@
+package com.activemesa.creational.builder.exercise;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import com.activemesa.creational.builder.exercise.CodeBuilder;
+
+public class Evaluate
+{
+  private String preprocess(String text)
+  {
+    return text.replace("\r\n", "\n").trim();
+  }
+
+  @Test
+  public void emptyTest() {
+    CodeBuilder cb = new CodeBuilder("Foo");
+    assertEquals("public class Foo\n{\n}",
+      preprocess(cb.toString()));
+  }
+
+  @Test
+  public void personTest()
+  {
+    CodeBuilder cb = new CodeBuilder("Person")
+      .addField("name", "String")
+      .addField("age", "int");
+    assertEquals("public class Person\n{\n" +
+      "  public String name;\n" +
+      "  public int age;\n}",
+      preprocess(cb.toString()));
+  }
+}
